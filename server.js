96a14c34eae255a5f18ca472e85dea45e01455d6
Austin LePorin <austinleporin@gmail.com>
2022-07-25 11:34:34 -0400

    File structure update

---
+++ server.js
@@ -0,0 +1,21 @@
+const courseController = require('./golf_server/controllers/courseController')
+
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+const db = require('./db')
+const Course = require('./models/course.js')
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+app.use(express.json())
+app.use(logger('dev'))
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/', routes)
+
+db.on('error', console.error.bind(console, 'MongoDB connection error!'))
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT} `))
