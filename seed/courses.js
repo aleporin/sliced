5e6b551c05d6591f4ca8a48f54468ec487834f94
Austin LePorin <austinleporin@gmail.com>
2022-07-25 20:01:06 -0400

    New Seeds

--- seed/courses.js
+++ seed/courses.js
@@ -4,7 +4,7 @@ const { Course } = require('../models')
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const main = async () => {
-  // const courseReviews = await Course.find({name: })
+  Course.collection.drop()
   const courses = [
     {
       name: 'Hamlet at Wind Watch',
@@ -14,8 +14,7 @@ const main = async () => {
       phone_num: '(631) 232-9850',
       description:
         'This superbly landscaped and maintained daily fee course is unique to Long Island with water potentially coming into play on thirteen holes.  Starting out at one of the highest points on Long Island, our elevated first hole will challenge your skills and capture your attention with stunning views of the North Shore of Long Island.  Our course provides a thoroughly enjoyable experience for all of our golfers with four sets of tees. The black tees play at 6,686 yards, the blue tees at 6,614 yards, the white tees at 6,030 yards, and the red tees at 5,178 yards.',
-      // reviews: [],
-      map_img: 'https://imgur.com/a/qj5OhgE'
+      map_img: 'assets/Screen Shot 2022-07-25 at 5.24.20 PM.png'
     },
     {
       name: 'Willow Creek Golf & Country Club ',
@@ -24,8 +23,34 @@ const main = async () => {
       img: 'https://www.invitedclubs.com/globalassets/willow-creek-golf--country-club/_images/willow-creek-clubhouse-18_1920.jpg?format=webp',
       phone_num: '(631) 474-9200',
       description: `The Management & Staff at Willow Creek Golf & Country Club want you and your golf league to experience Long Island's best kept secret today. Tucked away in beautiful Mount Sinai along the north shore of Long Island, Willow Creek is the premier destination for your golf. Let us help you spend more time practicing and less time planning. Not only can we host your next event, we can also help you secure other venues for your season like the stunning Wind Watch Golf & County Club in Hauppauge.The black tees play at 6,610 yards, the blue tees at 6,289 yards, the white tees at 5,810 yards, and the red tees at 5,013 yards.`,
-      // reviews: [],
-      map_img: 'https://imgur.com/C4XowyD'
+      map_img: 'assets/Screen Shot 2022-07-25 at 1.14.02 PM.png'
+    },
+    {
+      name: 'Mill Pond Golf Course ',
+      location: 'Medford, NY',
+      url: 'https://www.golfatmillpond.com/',
+      img: 'https://www.golfatmillpond.com/wp-content/uploads/sites/7798/2020/03/maxresdefault-1.jpg',
+      phone_num: '(631) 732-8249',
+      description: `Mill Pond Golf Course is a beautiful, 18 hole championship golf course located in Medford, NY on Long Island. Mill Pond offers the public golfer a rare treat: incredible golf coupled with superb service and all the amenities one would expect at a private club. The links style layout situated on over 200 acres is reminiscent of classic Scottish courses. Designed by Course Architect William “Buddy” Johnson, Mill Pond opened in 1999 and is recognized by Audubon International for Environmental Excellence. Online Tee Times can be made up to 7 days in advance. The blue tees play at 6,304 yards, the white tees at 5,966 yards, the gold tees at 5,156 yards, and the red tees at 4,831 yards.`,
+      map_img: 'assets/Screen Shot 2022-07-25 at 5.20.39 PM.png'
+    },
+    {
+      name: 'Spring Lake Golf Club',
+      location: 'Middle Island, NY',
+      url: 'https://www.springlakegolfclub.com/',
+      img: 'https://www.springlakegolfclub.com/images/widgetkit/course/course%20(3).jpg',
+      phone_num: '(631) 924-5115',
+      description: `Spring Lake Golf Course and Catering, a 27-hole public facility, is located in the heart of Long Island, NY in Middle Island. The New York golf course is just off the Long Island Expressway's exit 64 North and only minutes from the Port Jefferson Ferry. The sprawling 180-acre NY golf center is adjacent to a stunning lake providing eye dazzling views and features a perfect backdrop for your golf outing.The gold tees play at 7,035 yards, the blue tees at 6,445 yards, the white tees at 6,011 yards, and the red tees at 5,366 yards.`,
+      map_img: 'assets/Screen Shot 2022-07-25 at 5.29.01 PM.png'
+    },
+    {
+      name: 'Stonebridge Golf Links',
+      location: 'Hauppauge, NY',
+      url: 'https://www.stonebridgeglcc.com/',
+      img: 'https://www.golfstonebridgeutah.com/images/template/home-photo.jpg',
+      phone_num: '(631) 724-7500',
+      description: `The 18-hole "Stonebridge" course at the Stonebridge Golf Links & Country Club facility in Smithtown, New York features 6,245 yards of golf from the longest tees for a par of 70 . The course rating is 71.0 and it has a slope rating of 127.`,
+      map_img: 'assets/Screen Shot 2022-07-25 at 5.29.01 PM.png'
     }
   ]
   await Course.insertMany(courses)
