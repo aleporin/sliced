f549c7a168f01a23c6e38ce353ae5f6b44f53ce9
Austin LePorin <austinleporin@gmail.com>
2022-07-29 12:25:10 -0400

    Sliced Deploy

--- db/index.js
+++ db/index.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
-console.log(process.env.NODE_ENV)
 let dbUrl =
   process.env.NODE_ENV === 'production'
     ? process.env.MONGODB_URI
     : 'mongodb://127.0.0.1:27017/slicedDB'
+
+// let dbUrl = 'mongodb://127.0.0.1:27017/slicedDB'
 mongoose
   .connect(dbUrl)
   .then(() => {
